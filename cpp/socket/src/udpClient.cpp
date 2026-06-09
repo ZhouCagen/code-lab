d4d1d903f2a0c90f2450d953a0a5e2a2c6b27191
ZhouCagen <1660195531@qq.com>
2026-06-09 16:00:56 +0800

    Sync lab C++ code: 2026-06-09 16:00:47

---
+++ cpp/socket/src/udpClient.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+
+int main()
+{
+    std::cout << "UDP client test" << std::endl;
+    return 0;
+}
---
+++ cpp/socket/src/udpServer.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+
+int main()
+{
+    std::cout << "UDP server test" << std::endl;
+    return 0;
+}
