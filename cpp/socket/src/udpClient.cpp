318bb6221fe34c398ae8edd697dad3a85d7953dd
ZhouCagen <1660195531@qq.com>
2026-06-09 22:05:11 +0800

    Sync lab C++ code: 2026-06-09 22:05:11

--- cpp/socket/src/udpClient.cpp
+++ cpp/socket/src/udpClient.cpp
@@ -1,7 +1,47 @@
-#include <iostream>
+#include <arpa/inet.h>
+#include <netinet/in.h>
+#include <sys/socket.h>
+#include <unistd.h>
+
+#include <cerrno>
+#include <cstdlib>
+#include <cstring>
+#include <spdlog/spdlog.h>
+
+using namespace std;
 
 int main()
 {
-    std::cout << "UDP client test" << std::endl;
-    return 0;
+    const char* serverIp = "127.0.0.1";
+    const int serverPort = 2006;
+    const int bufferSize = 1024;
+
+    int clientSocket = socket(AF_INET, SOCK_DGRAM, 0);
+    if (clientSocket == -1)
+    {
+        spdlog::error("socket failed: {}", strerror(errno));
+        return EXIT_FAILURE;
+    }
+
+    spdlog::info("socket created successfully");
+    sockaddr_in clientAddr{};
+    clientAddr.sin_family = AF_INET;
+    clientAddr.sin_port = htons(serverPort);
+    int ipResult = inet_pton(AF_INET, serverIp, &clientAddr.sin_addr);
+
+    if (ipResult <= 0)
+    {
+        spdlog::error("inet_pton failed: {}", strerror(errno));
+        close(clientSocket);
+        return EXIT_FAILURE;
+    }
+
+    spdlog::info("UDP client started, server {}:{}", serverIp, serverPort);
+
+    string message;
+    char buffer[bufferSize];
+
+    while (1)
+    {
+    }
 }
